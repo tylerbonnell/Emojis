20d0ef8a9869df0d093d43d72f6a6c161381819e
Tyler Bonnell <tbonnell@uw.edu>
2016-03-14 16:16:12 -0700

    constructing map

--- translate.js
+++ translate.js
@@ -1,3 +1,5 @@
+var words = {};
+
 window.onload = function() {
   parseCSV();
   document.getElementById("output").innerHTML = "test";
@@ -5,9 +7,23 @@ window.onload = function() {
 
 function parseCSV() {
   Papa.parse("http://bonn.pw/Emojis/tags.csv", {
+    skipEmptyLines: true,
   	download: true,
   	complete: function(results) {
-  		console.log(results);
+  		for (var i = 0; i < results.data.length; i++) {
+        var row = results.data[i];
+        for (var j = 1; j < row.length; j++) {
+          mapWordToEmoji(row[j], row[0]);
+        }
+      }
   	}
   });
+}
+
+function mapWordToEmoji(word, emoji) {
+  if (word in words) {
+    words[word].push(emoji);
+  } else {
+    words[word] = [emoji];
+  }
 }
