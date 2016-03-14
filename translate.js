50cb4f8a744e8253ef11ca140275e3d1b20b1e46
Tyler Bonnell <tbonnell@uw.edu>
2016-03-14 16:53:26 -0700

    basically just reprints the text now

--- translate.js
+++ translate.js
@@ -1,10 +1,14 @@
+// Maps words to arrays of emojis
 var words = {};
 
 window.onload = function() {
   parseCSV();
   document.getElementById("output").innerHTML = "test";
+  document.getElementById("butt").onclick = translate;
 };
 
+// Fetch CSV file containing emojis/tags from the server
+// and parse it into the map for word lookup
 function parseCSV() {
   Papa.parse("http://bonn.pw/Emojis/tags.csv", {
     skipEmptyLines: true,
@@ -13,17 +17,34 @@ function parseCSV() {
   		for (var i = 0; i < results.data.length; i++) {
         var row = results.data[i];
         for (var j = 1; j < row.length; j++) {
-          mapWordToEmoji(row[j], row[0]);
+          mapWordToEmoji(row[j].toLowerCase(), row[0]);
         }
       }
   	}
   });
 }
 
+// Maps the given word to the emoji. If the word already
+// maps to an emoji, adds it to the array
 function mapWordToEmoji(word, emoji) {
   if (word in words) {
     words[word].push(emoji);
   } else {
     words[word] = [emoji];
   }
+}
+
+function translate() {
+  var input = document.getElementById("input").value;
+  var output = document.getElementById("output");
+  var rows = input.split(/\n/);
+  output.innerHTML = "";
+  for (var i = 0; i < rows.length; i++) {
+    output.innerHTML += translateRow(rows[i]) + "</br>";
+  }
+}
+
+function translateRow(str) {
+  var strWords = str.split(/\s+/);
+  return str;
 }
